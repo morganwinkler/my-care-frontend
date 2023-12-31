9499a2061d7ce2dbebb3778cb79100f45c6e943a
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-12-31 13:38:43 -0500

    Make home page and navbar conditional
    Renders components based on presence of jwt in local storage

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,13 +1,44 @@
+import axios from "axios";
 import { Login } from "./Login";
-import { Logout } from "./Logout";
 import { Signup } from "./Signup";
+import { useState, useEffect } from "react";
+import { Home } from "./Home";
+import { Routes, Route } from "react-router-dom";
 
 export function Content() {
+  const [visits, setVisits] = useState([]);
+  const [userLoggedIn, setUserLoggedIn] = useState(localStorage.jwt !== undefined);
+
+  const handleIndexVisits = () => {
+    axios.get("http://localhost:3000/visits.json").then((response) => {
+      setVisits(response.data);
+    });
+  };
+
+  let homePage;
+  if (!userLoggedIn) {
+    homePage = (
+      <div>
+        <Signup />
+        <Login setUserLoggedIn={setUserLoggedIn} />
+      </div>
+    );
+  } else {
+    homePage = (
+      <div>
+        <Home visits={visits} />
+      </div>
+    );
+  }
+
+  useEffect(() => {
+    if (userLoggedIn) {
+      handleIndexVisits();
+    }
+  }, [userLoggedIn]);
   return (
-    <div>
-      <Signup />
-      <Login />
-      <Logout />
-    </div>
+    <Routes>
+      <Route path="/" element={homePage} />
+    </Routes>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,41 @@
+import { useLocation, Link } from "react-router-dom";
+import { Logout } from "./Logout";
+
 export function Header() {
-  return (
-    <nav>
-      <h1>MyCare</h1>
-    </nav>
-  );
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  let navLinks;
+  if (localStorage.jwt === undefined) {
+    navLinks = (
+      <nav>
+        <h2>MyCare</h2>
+      </nav>
+    );
+  } else {
+    if (currentPath === "/") {
+      navLinks = (
+        <nav>
+          <h2>MyCare</h2>
+          <div>
+            <Logout />
+          </div>
+        </nav>
+      );
+    } else {
+      navLinks = (
+        <nav>
+          <h2>MyCare</h2>
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Logout />
+          </div>
+        </nav>
+      );
+    }
+  }
+
+  return <header>{navLinks}</header>;
 }
