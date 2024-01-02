bf7a5cba747103d618fd550b9b2dd0f144ed8beb
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-01-02 15:49:02 -0500

    Style header/footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <div>
+    <div className="footer footer-center p-4 bg-base-300 text-base-content">
       <p>Copyright 2023</p>
     </div>
   );
--- src/Header.jsx
+++ src/Header.jsx
@@ -8,28 +8,31 @@ export function Header() {
   let navLinks;
   if (localStorage.jwt === undefined) {
     navLinks = (
-      <nav>
-        <h2>MyCare</h2>
+      <nav className="navbar bg-base-300">
+        <h2 className="text-5xl">MyCare</h2>
       </nav>
     );
   } else {
     if (currentPath === "/") {
       navLinks = (
-        <nav>
-          <h2>MyCare</h2>
-          <div>
+        <nav className="navbar bg-base-300">
+          <h2 className=" text-5xl">MyCare</h2>
+          <div className="navbar-start"></div>
+          <div className="navbar-end hover:text-blue-600">
             <Logout />
           </div>
         </nav>
       );
     } else {
       navLinks = (
-        <nav>
-          <h2>MyCare</h2>
-          <div>
+        <nav className="navbar bg-base-300">
+          <h2 className=" text-5xl" style={{ paddingRight: "50px" }}>
+            MyCare
+          </h2>
+          <div className="navbar-start hover:text-blue-600">
             <Link to="/">Home</Link>
           </div>
-          <div>
+          <div className="navbar-end hover:text-blue-600">
             <Logout />
           </div>
         </nav>
