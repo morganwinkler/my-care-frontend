167a097e5b48e94b2e9bbc44f5c0a64376cbac3d
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-01-02 16:55:49 -0500

    Style home page and add visit modal

--- src/Home.jsx
+++ src/Home.jsx
@@ -35,25 +35,40 @@ export function Home(props) {
 
   return (
     <div>
-      <div>
-        <button onClick={handleShowVisitModal}>+ Visit</button>
+      <div className="flex flex-row-reverse">
+        <button onClick={handleShowVisitModal} className="btn btn-accent" style={{ margin: "15px" }}>
+          + Visit
+        </button>
         <AddVisitModal show={isVisitModalVisible} onClose={handleCloseVisitModal} onAddVisit={handleAddVisit} />
       </div>
-      <h2>My Visits</h2>
-      {props.visits && props.visits.length > 0 ? (
-        props.visits.map((visit) => (
-          <div key={visit.id}>
-            <p>Reason for admission: {visit.reason}</p>
-            <p>Hospital: {visit.hospital}</p>
-            <p>Admission Date: {visit.start_date}</p>
-            <button>
-              <Link to={`visits/${visit.id}`}>More Info</Link>
-            </button>
-          </div>
-        ))
-      ) : (
-        <p>You do not have any visits yet</p>
-      )}
+      <div className="grid-flow-row">
+        <h2 className="text-center text-3xl underline font-bold" style={{ paddingBottom: "50px" }}>
+          My Visits
+        </h2>
+        {props.visits && props.visits.length > 0 ? (
+          props.visits.map((visit) => (
+            <div
+              key={visit.id}
+              className="card shadow-2xl"
+              style={{ padding: "30px", marginBottom: "50px", marginLeft: "250px", marginRight: "250px" }}
+            >
+              <div className="grid grid-cols-3 text-center" style={{ paddingBottom: "30px" }}>
+                <p>Reason for admission: {visit.reason}</p>
+                <p>Hospital: {visit.hospital}</p>
+                <p>Admission Date: {visit.start_date}</p>
+              </div>
+              <div className="text-center">
+                <button className="btn btn-accent" style={{ maxWidth: "100px" }}>
+                  <Link to={`visits/${visit.id}`}>More Info</Link>
+                </button>
+              </div>
+              <div></div>
+            </div>
+          ))
+        ) : (
+          <p>You do not have any visits yet</p>
+        )}
+      </div>
     </div>
   );
 }
--- src/Modals/AddVisitModal.jsx
+++ src/Modals/AddVisitModal.jsx
@@ -24,20 +24,50 @@ export function AddVisitModal(props) {
 
   if (props.show) {
     return (
-      <div className="modal-background container">
-        <section className="modal-main">
-          <div className="modal-content">
-            <h2>Add New Visit</h2>
+      <div className="modal-background">
+        <section className="modal-box">
+          <div className="modal-content flex flex-col">
+            <h2 className="text-center text-xl font-bold">Add New Visit</h2>
             <label>Hospital:</label>
-            <input type="text" name="hospital" value={visitData.hospital} onChange={handleChange} />
+            <input
+              type="text"
+              name="hospital"
+              value={visitData.hospital}
+              onChange={handleChange}
+              className="input input-bordered input-accent "
+            />
             <label>Date of Admission:</label>
-            <input type="date" name="start_date" value={visitData.start_date} onChange={handleChange} />
+            <input
+              type="date"
+              name="start_date"
+              value={visitData.start_date}
+              onChange={handleChange}
+              className="input input-bordered input-accent "
+            />
             <label>Date of Discharge:</label>
-            <input type="date" name="end_date" value={visitData.end_date} onChange={handleChange} />
+            <input
+              type="date"
+              name="end_date"
+              value={visitData.end_date}
+              onChange={handleChange}
+              className="input input-bordered input-accent "
+            />
             <label>Reason for Admission:</label>
-            <input type="text" name="reason" value={visitData.reason} onChange={handleChange} />
-            <button onClick={handleAddVisit}>Add Visit</button>
-            <button onClick={props.onClose}>Cancel</button>
+            <input
+              type="text"
+              name="reason"
+              value={visitData.reason}
+              onChange={handleChange}
+              className="input input-bordered input-accent "
+            />
+            <div className="text-center" style={{ padding: "10px" }}>
+              <button className="btn btn-outline btn-success" onClick={handleAddVisit} style={{ margin: "5px" }}>
+                Add Visit
+              </button>
+              <button className="btn btn-outline btn-error" onClick={props.onClose}>
+                Cancel
+              </button>
+            </div>
           </div>
         </section>
       </div>
