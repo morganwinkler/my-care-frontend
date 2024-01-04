ba21b640c1534e179e3ae1cbcc85abb088b38ae0
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-01-04 17:05:23 -0500

    Style add procedure modal

--- src/Modals/AddProcedureModal.jsx
+++ src/Modals/AddProcedureModal.jsx
@@ -31,22 +31,67 @@ export function AddProcedureModal(props) {
 
   if (props.show) {
     return (
-      <div className="modal-background container">
+      <div className="modal-background">
         <section className="modal-main">
-          <div className="modal-content">
-            <h2>Add New Procedure</h2>
-            <label>Name:</label>
-            <input type="text" name="name" value={procedureData.name} onChange={handleChange} />
-            <label>Date:</label>
-            <input type="date" name="date" value={procedureData.date} onChange={handleChange} />
-            <label>Reason:</label>
-            <input type="text" name="reason" value={procedureData.reason} onChange={handleChange} />
-            <label>Result:</label>
-            <input type="text" name="result" value={procedureData.result} onChange={handleChange} />
-            <label>Note:</label>
-            <textarea name="note" value={procedureData.note} onChange={handleChange} />
-            <button onClick={handleAddProcedure}>Add Procedure</button>
-            <button onClick={props.onClose}>Cancel</button>
+          <div className="flex justify-center">
+            <div className="prose">
+              <div className="modal-content">
+                <h2>Add New Procedure</h2>
+                <div className="flex flex-col">
+                  <label>Name:</label>
+                  <input
+                    type="text"
+                    name="name"
+                    className="input input-bordered input-accent"
+                    value={procedureData.name}
+                    onChange={handleChange}
+                  />
+                  <label>Date:</label>
+                  <input
+                    type="date"
+                    name="date"
+                    className="input input-bordered input-accent"
+                    value={procedureData.date}
+                    onChange={handleChange}
+                  />
+                  <label>Reason:</label>
+                  <input
+                    type="text"
+                    name="reason"
+                    className="input input-bordered input-accent"
+                    value={procedureData.reason}
+                    onChange={handleChange}
+                  />
+                  <label>Result:</label>
+                  <input
+                    type="text"
+                    name="result"
+                    className="input input-bordered input-accent"
+                    value={procedureData.result}
+                    onChange={handleChange}
+                  />
+                  <label>Note:</label>
+                  <textarea
+                    name="note"
+                    className="input input-bordered input-accent"
+                    value={procedureData.note}
+                    onChange={handleChange}
+                  />
+                  <div className="text-center" style={{ padding: "10px" }}>
+                    <button
+                      className="btn btn-outline btn-success"
+                      style={{ margin: "5px" }}
+                      onClick={handleAddProcedure}
+                    >
+                      Add Procedure
+                    </button>
+                    <button className="btn btn-outline btn-error" onClick={props.onClose}>
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </div>
