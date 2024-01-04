f116d3995c738019f002a9430b3da8b225cd7da9
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-01-04 17:07:22 -0500

    Style add question modal

--- src/Modals/AddQuestionModal.jsx
+++ src/Modals/AddQuestionModal.jsx
@@ -23,18 +23,51 @@ export function AddQuestionModal(props) {
 
   if (props.show) {
     return (
-      <div className="modal-background container">
+      <div className="modal-background">
         <section className="modal-main">
-          <div className="modal-content">
-            <h2>Add New Question</h2>
-            <label>Question:</label>
-            <input type="text" name="question" value={questionData.name} onChange={handleChange} />
-            <label>Answer:</label>
-            <input type="text" name="answer" value={questionData.reason} onChange={handleChange} />
-            <label>Note:</label>
-            <textarea name="note" value={questionData.note} onChange={handleChange} />
-            <button onClick={handleAddQuestion}>Add Question</button>
-            <button onClick={props.onClose}>Cancel</button>
+          <div className="flex justify-center">
+            <div className="prose">
+              <div className="modal-content">
+                <h2>Add New Question</h2>
+                <div className="flex flex-col">
+                  <label>Question:</label>
+                  <input
+                    type="text"
+                    name="question"
+                    className="input input-bordered input-accent"
+                    value={questionData.name}
+                    onChange={handleChange}
+                  />
+                  <label>Answer:</label>
+                  <input
+                    type="text"
+                    name="answer"
+                    className="input input-bordered input-accent"
+                    value={questionData.reason}
+                    onChange={handleChange}
+                  />
+                  <label>Note:</label>
+                  <textarea
+                    name="note"
+                    className="input input-bordered input-accent"
+                    value={questionData.note}
+                    onChange={handleChange}
+                  />
+                  <div className="text-center" style={{ padding: "10px" }}>
+                    <button
+                      className="btn btn-outline btn-success"
+                      style={{ margin: "5px" }}
+                      onClick={handleAddQuestion}
+                    >
+                      Add Question
+                    </button>
+                    <button className="btn btn-outline btn-error" onClick={props.onClose}>
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </div>
