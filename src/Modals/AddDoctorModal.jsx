70a026e9ca2c3da94c994feb6d732bc9e2b4dd8e
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-01-04 16:49:29 -0500

    Style add doctor modal

--- src/Modals/AddDoctorModal.jsx
+++ src/Modals/AddDoctorModal.jsx
@@ -23,18 +23,47 @@ export function AddDoctorModal(props) {
 
   if (props.show) {
     return (
-      <div className="modal-background container">
-        <section className="modal-main">
+      <div className="modal-background">
+        <section className="modal-box">
           <div className="modal-content">
-            <h2>Add New Doctor</h2>
-            <label>Name:</label>
-            <input type="text" name="name" value={doctorData.name} onChange={handleChange} />
-            <label>Specialty:</label>
-            <input type="text" name="specialty" value={doctorData.specialty} onChange={handleChange} />
-            <label>Note:</label>
-            <textarea name="note" value={doctorData.note} onChange={handleChange} />
-            <button onClick={handleAddDoctor}>Add Doctor</button>
-            <button onClick={props.onClose}>Cancel</button>
+            <div className="prose">
+              <div>
+                <h2>Add New Doctor</h2>
+              </div>
+              <div className="flex flex-col">
+                <label>Name:</label>
+                <input
+                  type="text"
+                  name="name"
+                  className="input input-bordered input-accent"
+                  value={doctorData.name}
+                  onChange={handleChange}
+                />
+                <label>Specialty:</label>
+                <input
+                  type="text"
+                  name="specialty"
+                  className="input input-bordered input-accent"
+                  value={doctorData.specialty}
+                  onChange={handleChange}
+                />
+                <label>Note:</label>
+                <textarea
+                  name="note"
+                  className="input input-bordered input-accent"
+                  value={doctorData.note}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="text-center" style={{ padding: "10px" }}>
+                <button className="btn btn-outline btn-success" style={{ margin: "5px" }} onClick={handleAddDoctor}>
+                  Add Doctor
+                </button>
+                <button className="btn btn-outline btn-error" onClick={props.onClose}>
+                  Cancel
+                </button>
+              </div>
+            </div>
           </div>
         </section>
       </div>
