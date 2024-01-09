a3c5b8e49f61058fbb521077d6c7244b19faea70
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-01-09 11:10:34 -0500

    Add button to add article to user library

--- src/Library.jsx
+++ src/Library.jsx
@@ -5,6 +5,28 @@ export function Library() {
   const [data, setData] = useState(null);
   const [keyword, setKeyword] = useState("");
 
+  const handleKeywordChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  const handleMoreInfoClick = (url) => {
+    window.open(url, "_blank");
+  };
+
+  const handleAddToLibrary = (article) => {
+    axios
+      .post("http://localhost:3000/articles.json", {
+        title: article.Title,
+        image_url: article.ImageUrl,
+        link: article.AccessibleVersion,
+      })
+      .then((response) => {
+        console.log("Article added to library:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error adding article to library:", error);
+      });
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -20,13 +42,6 @@ export function Library() {
     fetchData();
   }, [keyword]);
 
-  const handleKeywordChange = (event) => {
-    setKeyword(event.target.value);
-  };
-  const handleMoreInfoClick = (url) => {
-    window.open(url, "_blank");
-  };
-
   return (
     <div>
       <label>
@@ -40,7 +55,7 @@ export function Library() {
               <h3>{resource.Title}</h3>
               <img src={resource.ImageUrl} alt={resource.ImageAlt} />
               <button onClick={() => handleMoreInfoClick(resource.AccessibleVersion)}>More Information</button>
-              <button>Add To My Library</button>
+              <button onClick={() => handleAddToLibrary(resource)}>Add To My Library</button>
             </div>
           ))}
         </ul>
